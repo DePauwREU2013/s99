14d9f7f3f163074ac04b52f43cb82fac1503d479
Mervin Fansler <mmfansler@gmail.com>
2013-06-18 12:45:21 -0400

    Solved 46-49 Logic

--- src/main/scala/s99/LogicAndCodesSolutions.scala
+++ src/main/scala/s99/LogicAndCodesSolutions.scala
@@ -16,18 +16,37 @@ trait LogicAndCodesSolutions { outer =>
 
   }
   
-  def and(a: Boolean,  b: =>Boolean): Boolean = ???
-  def or(a: Boolean,   b: =>Boolean): Boolean = ???
-  def nand(a: Boolean,  b: =>Boolean): Boolean = ???
-  def nor(a: Boolean,  b: =>Boolean): Boolean = ???
-  def xor(a: Boolean,  b: =>Boolean): Boolean = ???
-  def impl(a: Boolean,  b: =>Boolean): Boolean = ???
-  def equ(a: Boolean,  b: =>Boolean): Boolean = ???
-  def not(a: Boolean) = ???
+  def and(a: Boolean,  b: =>Boolean): Boolean = if (a) b else false
+  def or(a: Boolean,   b: =>Boolean): Boolean = if (a) true else b
+  def nand(a: Boolean,  b: =>Boolean): Boolean = ! (a and b)
+  def nor(a: Boolean,  b: =>Boolean): Boolean = if (a) false else !b
+  def xor(a: Boolean,  b: =>Boolean): Boolean = (a or b) and !(a and b)
+  def impl(a: Boolean,  b: =>Boolean): Boolean = if (!a) true else b
+  def equ(a: Boolean,  b: =>Boolean): Boolean = (a impl b) and (b impl a)
+  def not(a: Boolean) = !a
   
-  def table2(f: (Boolean, Boolean) => Boolean): String = ???
-
-  def gray(n: Int): List[String] = ???
+  def table2(f: (Boolean, Boolean) => Boolean): String = {
+    "A     B     result" +
+    outerProduct(List(true, false)).map(xs => xs match { 
+      case a :: b :: Nil => "\n" + a.toString.padTo(6, ' ') + b.toString.padTo(6, ' ') +  f(a,b).toString.padTo(6, ' ')
+      case _ => ""
+    }).mkString
+  }
+  
+  def grayStream: Stream[List[String]] = {
+    Stream.iterate(List(""))(xs => xs.map('0'+_) ++: xs.map('1'+_).reverse)
+  }
+  
+  def gray(n: Int): List[String] = grayStream(n)
+  
   def huffman(list: List[(String,  Int)]): List[(String, String)] = ???
 
+  def cross[A](xs:List[A], ys: List[A]): List[List[A]] = {
+    for {
+      x <- xs
+      y <- ys
+    } yield List(x,y)
+  }
+  
+  def outerProduct[A](xs:List[A]): List[List[A]] = cross(xs, xs)
 }
